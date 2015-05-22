e9dc56651ff570097345865738ee614d79eeba58
CarolinaManzanares <mczanares@gmail.com>
2015-05-22 16:02:04 +0200

    Custom Directive

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,7 +2,14 @@ angular.module('starter.controllers', ['inspirapp.db'])
 
 .controller('DashCtrl', ['$scope', 'db', function($scope, db) {
 
-	db.get('3', function(data){
+	db.get('1', function(data){
+		$scope.type= data.type;
+		console.log(data.type);
+		if ($scope.type === 'formattext')
+		{
+			$scope.formattext = data.inspiration.split('|');
+			console.log($scope.formattext);
+		}
 		$scope.inspiration = data.inspiration;
 		$scope.$apply();
 
@@ -16,4 +23,34 @@ angular.module('starter.controllers', ['inspirapp.db'])
   $scope.settings = {
     enableFriends: true
   };
+})
+
+.directive('formatText', function(){
+	return{
+		restrict: 'E',
+		templateUrl: '../templates/format-text-view.html',
+		//controller: 'DashCtrl'
+
+	}
+
+})
+
+.directive('mp3', function(){
+	return{
+		restrict: 'E',
+		template: '<h1>mp3 Directive</h1>',
+		//controller: 'DashCtrl'
+
+	}
+
+})
+
+.directive('text', function(){
+	return{
+		restrict: 'E',
+		template: '<h1>text Directive</h1>',
+		//controller: 'DashCtrl'
+
+	}
+
 });
